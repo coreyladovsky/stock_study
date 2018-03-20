fa55e3ecba6c0ef66d618adf8a5b62b341bf695f
Corey Ladovsky <coreyskymusic@gmail.com>
2018-03-20 17:54:53 -0400

    all lines being drawn

--- bundle.js
+++ bundle.js
@@ -76,175 +76,152 @@ document.addEventListener("DOMContentLoaded", () => {
   defaultStocks();
 });
 
-const defaultStocks =  () => {
-  let defaults = ["ROBO", "XLK", "VGT", "FDN", "IYW"]
-  let promises = []
+const defaultStocks = () => {
+  // let defaults = ["ROBO", "XLK", "VGT", "FDN", "IYW"]
+  let defaults = ["CHFS", "SLS", "NFEC", "MRNS", "NOG"];
+  let promises = [];
   for (var i = 0; i < defaults.length; i++) {
-    promises.push(fetchStock(defaults[i]))
+    promises.push(fetchStock(defaults[i]));
   }
-  Promise.all(promises).then((results) => {
-    let stockData = results.map((res) => {
-      return Object(__WEBPACK_IMPORTED_MODULE_0__cleanData_js__["a" /* cleanerData */])(res)
-    })
-    
-    const data = Object(__WEBPACK_IMPORTED_MODULE_0__cleanData_js__["b" /* singleStock */])(stockData[0])
-    const data2 = Object(__WEBPACK_IMPORTED_MODULE_0__cleanData_js__["b" /* singleStock */])(stockData[1])
+  Promise.all(promises).then(results => {
+    let stockData = results.map(res => {
+      return Object(__WEBPACK_IMPORTED_MODULE_0__cleanData_js__["a" /* cleanerData */])(res);
+    });
 
-    const margin = { top: 20, bottom: 100, left: 50, right: 10};
+    const data = Object(__WEBPACK_IMPORTED_MODULE_0__cleanData_js__["b" /* singleStock */])(stockData[0]);
 
-    const width = 700 - margin.right - margin.left ;
-    const height = 500 - margin.top - margin.bottom ;
+    const margin = { top: 30, bottom: 50, left: 50, right: 10 };
+
+    const width = 700 - margin.right - margin.left;
+    const height = 500 - margin.top - margin.bottom;
     const color = d3.scaleOrdinal(d3.schemeCategory10);
-
 
     data.forEach(function(d) {
       d.date = d.date;
       d.close = +d.close;
       d.ticker = d.ticker;
-    })
+    });
 
     const makeSvg = () => {
-      return  d3.select("#root").append("svg")
-      .attr("height", height + margin.left + margin.right)
-      .attr("width", width + margin.top + margin.bottom)
-      .attr("class", "svg-all-stocks")
-    }
+      return d3
+        .select("#root")
+        .append("svg")
+        .attr("height", height + margin.left + margin.right + 50)
+        .attr("width", width + margin.top + margin.bottom)
+        .attr("class", "svg-all-stocks");
+    };
 
     const maxAndMin = () => {
       let max;
       let min;
       for (let i = 0; i < stockData.length; i++) {
         for (var j = 0; j < stockData[i].length; j++) {
-          if(!min || parseInt(stockData[i][j]["4. close"]) < min) {
-            min = parseInt(stockData[i][j]["4. close"])
+          if (!min || parseInt(stockData[i][j]["4. close"]) < min) {
+            min = parseInt(stockData[i][j]["4. close"]);
           }
 
-          if(!max || parseInt(stockData[i][j]["4. close"]) > max) {
-            max = parseInt(stockData[i][j]["4. close"])
+          if (!max || parseInt(stockData[i][j]["4. close"]) > max) {
+            max = parseInt(stockData[i][j]["4. close"]);
           }
-
         }
       }
-      return [max, min]
-    }
+      return [max, min];
+    };
 
     var [max, min] = maxAndMin();
-    console.log(data);
-
-    // const x = d3.scaleTime().range([0, width])
-    // .domain(d3.extent(data, function(d) {return d.date}))
 
     var x = d3
-    .scaleBand()
-    .rangeRound([0, width])
-    .padding(0.1)
-    .domain(
-      data.map(function(d) {
-        return d.date;
-      })
-    );
+      .scaleBand()
+      .rangeRound([0, width])
+      .padding(0.1)
+      .domain(
+        data.map(function(d) {
+          return d.date;
+        })
+      );
 
-    const y = d3.scaleLinear()
-    .range([0, height])
-    .domain([min, max])
-
+    const y = d3
+      .scaleLinear()
+      .range([0, height])
+      .domain([0, max]);
 
     var y2 = d3
       .scaleLinear()
       .rangeRound([0, height])
-      .domain([
-        max, min
-      ]);
-//
+      .domain([max, 0]);
+    //
 
-    const drawLine = d3.line()
-        .x(function(d) {
-          return x(d.date)})
-        .y(function(d) { return height - y(d.close)})
+    const drawLine = d3
+      .line()
+      // .curve(d3.curveBasis)
+      .x(function(d) {
+        return x(d.date);
+      })
+      .y(function(d) {
+        return height + margin.top - y(d.close);
+      });
 
-        // x.domain(d3.extent(data, function(d) {return d.date}))
-        // y.domain([0, d3.max(data, function(d) { return d.close; })])
-    // console.log(stockData);
-      let svg = makeSvg();
 
-      let g = svg.append("g")
-      .attr("tranform", "translate(" + (50) + "," +  10 + ")")
+    let svg = makeSvg();
 
-  //     g
-  // .selectAll("path")
-  // .data(data)
-  // .enter()
-  // .append("path")
-  // .attr("class", "bar")
-  // .attr("x", function(d) {
-  //   return x(d.date);
-  // })
-  // .attr("y", function(d) {
-  //   return 425 - y(d.close);
-  // })
-  // .attr("width", x.bandwidth())
-  // .attr("height", function(d) {
-  //   return y(d.close);
-  // })
-  // .attr("fill", "blue");
+    let g = svg
+      .append("g")
+      .attr("tranform", "translate(" + 50 + "," + 10 + ")");
 
-      g.append("path")
+    g
+      .append("path")
+      .data([data])
+      .attr("class", "line")
+      .attr("d", drawLine)
+      .attr("transform", "translate(40," + margin.top + ")")
+      .attr("stroke", function(d) {
+        return color(d[0].ticker);
+      })
+      .attr("stroke-width", "2px")
+      .attr("fill", "none");
+
+    //
+    g
+      .append("g")
+      .attr("class", "x-axis")
+      .attr("transform", "translate(0, " + (height + margin.top) + ")")
+      .call(d3.axisBottom(x));
+    //
+    g
+      .append("g")
+      .attr("class", "y-axis")
+      .attr("transform", "translate(50," + margin.top + ")")
+      .call(d3.axisLeft(y2));
+
+    const makePath = (g, data) => {
+      g
+        .append("path")
         .data([data])
-        .attr("class", "line")
-        .attr("d", drawLine)
-        .attr("transform", "translate(40," + (margin.top) + ")")
-        .attr("stroke", function(d) {
-           return color(d[0].ticker)})
-        .attr("stroke-width", "2px")
-        .attr("fill", "none")
-
-      //
-        g.append("g")
-          .attr("class", "x-axis")
-          .attr("transform", "translate(0, " +  (height + margin.top) + ")")
-          .call(d3.axisBottom(x));
-      //
-        g.append("g")
-        .attr("class", "y-axis")
-        .attr("transform", "translate(50," + margin.top + ")")
-          .call(d3.axisLeft(y2));
-
-        g.append("path")
-        .data([data2])
         .attr("d", drawLine)
         .attr("class", "line2")
-        .attr("transform", "translate(40," + (margin.top) + ")")
+        .attr("transform", "translate(40," + margin.top + ")")
         .attr("stroke", function(d) {
-           return color(d[0].ticker)})
+          return color(d[0].ticker);
+        })
         .attr("stroke-width", "2px")
-        .attr("fill", "none")
+        .attr("fill", "none");
+    };
 
+    for (var i = 1; i < stockData.length; i++) {
+      let data = Object(__WEBPACK_IMPORTED_MODULE_0__cleanData_js__["b" /* singleStock */])(stockData[i]);
+      makePath(g, data);
+    }
 
-      // makeG(svg)
-    //   .selectAll(".all-stocks")
-    //   .data(data)
-    //   .enter()
-    //   .append("path")
-    //   .attr("class", "all-stocks")
-    //   .attr("d", drawLine)
-    //   .attr("stroke", "blue")
-    //   .attr("stroke-width", "5")
-  })
-}
+  });
+};
 
-
-
-
-
-const makeG = (svg) => {
- return svg.append("g")
- .attr("tranform", "translate(" + margin.left + "," +  margin.top + ")")
- .attr("height", height)
- .attr("width", width)
-}
-
-
-
+const makeG = svg => {
+  return svg
+    .append("g")
+    .attr("tranform", "translate(" + margin.left + "," + margin.top + ")")
+    .attr("height", height)
+    .attr("width", width);
+};
 
 const fetchStock = async ticker => {
   let data = await (await fetch(
